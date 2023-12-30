a0b978effa5e8c97e390a6bab6d22daaa7d48679
sergeyzoloto <sergiozoloto@gmail.com>
2023-12-30 16:36:41 +0100

    first simple test in react vite

---
+++ client/setupTests.js
@@ -0,0 +1,6 @@
+/**
+ * This file will be imported in all tests automatically by Jest
+ */
+
+// For react-testing-library, it will be the DOM mock
+import '@testing-library/jest-dom';
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
       <h1 className="text-3xl font-bold underline">Vite + React</h1>
+    </>
+  );
+  /*
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
-    </>
-  );
+  */
 }
 
 export default App;

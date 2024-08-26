b5dd16c344e7d4a162363cea11d21bafbc5d0c3b
sergeyzoloto <sergiozoloto@gmail.com>
2024-08-26 17:41:47 +0200

    add frontend configuration files and assets

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Include TypeScript files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

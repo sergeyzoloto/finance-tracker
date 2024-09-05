d165d33e395b44869abdb41f2f45b92315e7d931
sergeyzoloto <sergiozoloto@gmail.com>
2024-09-05 19:30:54 +0200

    first shadcn components installed, configured and rendered

---
+++ frontend/src/AppWrapper.tsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+import { BrowserRouter as Router } from "react-router-dom";
+
+/**
+ * This component wraps our App with the providers
+ * that we do not want to have in our tests
+ */
+function AppWrapper({ children }: { readonly children: React.ReactNode }) {
+  return <Router>{children}</Router>;
+}
+
+AppWrapper.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default AppWrapper;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig, loadEnv } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    define: {
-      "process.env.BASE_SERVER_URL": JSON.stringify(env.BASE_SERVER_URL),
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    plugins: [react()],
-  };
+  },
 });

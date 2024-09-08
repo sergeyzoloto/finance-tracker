13c252b4d488c5b6d81a5133157a8a219974cbf2
sergeyzoloto <sergiozoloto@gmail.com>
2024-09-08 19:54:57 +0200

    separate user context and reducer

--- frontend/src/context/userContext.tsx
+++ frontend/src/context/userContext.tsx
@@ -1,18 +1,6 @@
 import { createContext, useReducer, ReactNode, Dispatch, useMemo } from "react";
 
-// Define the shape of the user state
-interface UserState {
-  isAuthenticated: boolean;
-  user: { name: string } | null;
-}
-
-// Define the shape of the actions
-type Action =
-  | {
-      type: "LOGIN";
-      payload: { name: string };
-    }
-  | { type: "LOGOUT" };
+import userReducer, { UserState, Action } from "./userReducer";
 
 // Define the initial state
 const initialState: UserState = {
@@ -28,24 +16,6 @@ const UserContext = createContext<{
   state: initialState,
   dispatch: () => null,
 });
-
-// Define the reducer function
-const userReducer = (state: UserState, action: Action): UserState => {
-  switch (action.type) {
-    case "LOGIN":
-      return {
-        isAuthenticated: true,
-        user: { name: action.payload.name },
-      };
-    case "LOGOUT":
-      return {
-        isAuthenticated: false,
-        user: null,
-      };
-    default:
-      return state;
-  }
-};
 
 // Create a provider component
 const UserProvider = ({ children }: { children: ReactNode }) => {
